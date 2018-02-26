8b626daf6e63900c8db08e11281ac3de0e1092e1
Stephanie Cheng <stephanieccheng@gmail.com>
2018-02-26 12:35:12 -0800

    Added implementation for 10.1

--- 10-binary-trees/10.1-binary-tree-is-height-balanced.playground/Contents.swift
+++ 10-binary-trees/10.1-binary-tree-is-height-balanced.playground/Contents.swift
@@ -16,8 +16,42 @@ class BinaryTreeNode {
     }
 }
 
-func isBalanced(root: BinaryTreeNode) -> Bool {
-    
-    return false
+func isBalanced(root: BinaryTreeNode?) -> Bool {
+    return isBalancedInitial(root: root)
 }
 
+func isBalancedInitial(root: BinaryTreeNode?) -> Bool {
+    guard let root = root else {
+        return true
+    }
+    
+    let left = height(root.left, 0)
+    let right = height(root.right, 0)
+    
+    return left - right <= 1
+}
+
+func height(_ node: BinaryTreeNode?, _ currentHeight: Int) -> Int {
+    guard let node = node else {
+        return currentHeight + 1
+    }
+    return max(height(node.left, currentHeight + 1), height(node.right, currentHeight + 1))
+}
+
+// Testing
+let root = BinaryTreeNode(value: 3)
+let l1 = BinaryTreeNode(value: 1)
+let l2 = BinaryTreeNode(value: 2)
+let r1 = BinaryTreeNode(value: 4)
+
+assert(true == isBalanced(root: nil))
+assert(true == isBalanced(root: root))
+
+root.left = l2
+assert(true == isBalanced(root: root))
+
+l2.left = l1
+assert(false == isBalanced(root: root))
+
+root.right = r1
+assert(true == isBalanced(root: root))
